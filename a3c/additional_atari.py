c361e7e56cb4f86c96b45bd2014f6219db0c6651
Raphael Stäbler <raphael@productionbuild.de>
2018-10-12 10:21:25 +0200

    Updated atari adventure registration

--- a3c/additional_atari.py
+++ a3c/additional_atari.py
@@ -5,6 +5,6 @@ def register_adventure():
         id='Adventure-v0',
         entry_point='gym.envs.atari:AtariEnv',
         kwargs={'game': 'adventure', 'obs_type': 'image', 'repeat_action_probability': 0.25},
-        max_episode_steps=10000,
-        nondeterministic=False
+        max_episode_steps=None,
+        nondeterministic=True
     )
