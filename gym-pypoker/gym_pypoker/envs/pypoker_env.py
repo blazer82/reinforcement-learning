c770f8e01bc56b1e335024d01bdf7fa41e6877e9
Raphael Stäbler <raphael@productionbuild.de>
2018-10-14 20:58:43 +0200

    PyPoker environment on work

--- gym-pypoker/gym_pypoker/envs/pypoker_env.py
+++ gym-pypoker/gym_pypoker/envs/pypoker_env.py
@@ -1,7 +1,9 @@
 import gym
 import numpy
+import uuid
 from gym import error, spaces, utils
 from gym.utils import seeding
+from pypokerengine.api.emulator import Emulator
 
 class PyPokerEnv(gym.Env):
     metadata = {'render.modes': ['human']}
@@ -14,7 +16,16 @@ class PyPokerEnv(gym.Env):
         self.action_space = spaces.Tuple((
             spaces.Discrete(3),
             spaces.Box(low=0, high=1, shape=(1,), dtype=numpy.float32)
-            ))
+        ))
+
+        self.max_round = 10
+        self.initial_stack = 200
+        self.small_blind_amount = 5
+
+        self.random_player_uuid = uuid.uuid4()
+        self.gym_player_uuid = uuid.uuid4()
+
+        self.game_started = False
 
     def step(self, action):
         """
@@ -46,10 +57,25 @@ class PyPokerEnv(gym.Env):
                     use this for learning.
         """
         
+        if self.game_started is False:
+            self.game_state, events = self.emulator.start_new_round(self.game_state)
+            print(self.game_state)
+            print(self.game_state["table"].sb_pos())
+            print(self.game_state["table"].bb_pos())
+            return numpy.zeros((32,)), 0.0, False, {}
+
         return numpy.zeros((32,)), 0.0, False, {}
 
     def reset(self):
-        pass
+        self.emulator = Emulator()
+        self.emulator.set_game_rule(player_num=2, max_round=self.max_round, small_blind_amount=self.small_blind_amount, ante_amount=0)
+        
+        self.game_state = self.emulator.generate_initial_game_state(
+            {
+                self.random_player_uuid: { "name": "random_player", "stack": self.initial_stack },
+                self.gym_player_uuid: { "name": "gym_player", "stack": self.initial_stack }
+            }
+        )
 
     def render(self, mode='human', close=False):
         pass
