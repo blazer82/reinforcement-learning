393586db3d6ee9c06e26debdecbf77209a767009
Raphael Stäbler <raphael@productionbuild.de>
2018-10-15 17:47:15 +0200

    PyPoker environment on work

--- gym-pypoker/gym_pypoker/envs/pypoker_env.py
+++ gym-pypoker/gym_pypoker/envs/pypoker_env.py
@@ -4,6 +4,7 @@ import uuid
 from gym import error, spaces, utils
 from gym.utils import seeding
 from pypokerengine.api.emulator import Emulator
+from pypokerengine.engine.card import Card
 
 class PyPokerEnv(gym.Env):
     metadata = {'render.modes': ['human']}
@@ -22,8 +23,8 @@ class PyPokerEnv(gym.Env):
         self.initial_stack = 200
         self.small_blind_amount = 5
 
-        self.random_player_uuid = uuid.uuid4()
         self.gym_player_uuid = uuid.uuid4()
+        self.gym_player_pos = 1
 
         self.game_started = False
 
@@ -58,13 +59,12 @@ class PyPokerEnv(gym.Env):
         """
         
         if self.game_started is False:
+            self.game_started = True
             self.game_state, events = self.emulator.start_new_round(self.game_state)
             print(self.game_state)
-            print(self.game_state["table"].sb_pos())
-            print(self.game_state["table"].bb_pos())
-            return numpy.zeros((32,)), 0.0, False, {}
+            return self._get_observation(self.game_state), 0.0, False, {}
 
-        return numpy.zeros((32,)), 0.0, False, {}
+        return self._get_observation(self.game_state), 0.0, False, {}
 
     def reset(self):
         self.emulator = Emulator()
@@ -72,10 +72,43 @@ class PyPokerEnv(gym.Env):
         
         self.game_state = self.emulator.generate_initial_game_state(
             {
-                self.random_player_uuid: { "name": "random_player", "stack": self.initial_stack },
+                uuid.uuid4(): { "name": "random_player", "stack": self.initial_stack },
                 self.gym_player_uuid: { "name": "gym_player", "stack": self.initial_stack }
             }
         )
 
     def render(self, mode='human', close=False):
         pass
+
+    def _get_observation(self, game_state):
+        observation = numpy.zeros((32,), dtype=numpy.float32)
+        
+        # Game
+        observation[0] = game_state["table"].seats.size() / 4.
+        observation[1] = game_state["table"].seats.count_active_players() / game_state["table"].seats.size()
+        observation[2] = float(self.gym_player_pos) / game_state["table"].seats.size()
+        observation[3] = float(game_state["table"].sb_pos()) / game_state["table"].seats.size()
+        observation[4] = float(game_state["table"].bb_pos()) / game_state["table"].seats.size()
+
+        # Player cards
+        i = 5
+        for c, card in enumerate(self.game_state["table"].seats.players[self.gym_player_pos].hole_card):
+            observation[i + c * 5 + 0] = 1. if card.suit == Card.CLUB else 0.
+            observation[i + c * 5 + 1] = 1. if card.suit == Card.DIAMOND else 0.
+            observation[i + c * 5 + 2] = 1. if card.suit == Card.HEART else 0.
+            observation[i + c * 5 + 3] = 1. if card.suit == Card.SPADE else 0.
+            observation[i + c * 5 + 4] = card.rank / 14.
+
+        # Table
+        i = 15
+        for c, card in enumerate(self.game_state["table"].get_community_card()):
+            observation[i + c * 5 + 0] = 1. if card.suit == Card.CLUB else 0.
+            observation[i + c * 5 + 1] = 1. if card.suit == Card.DIAMOND else 0.
+            observation[i + c * 5 + 2] = 1. if card.suit == Card.HEART else 0.
+            observation[i + c * 5 + 3] = 1. if card.suit == Card.SPADE else 0.
+            observation[i + c * 5 + 4] = card.rank / 14.
+
+        return observation
+
+
+
